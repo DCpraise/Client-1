48a922b16fc1e9bc302f813cd8baded808ea064c
DCpraise <chukwupraise72@gmail.com>
2025-03-14 09:23:17 +0100

    name change

--- client/src/pages/ProductDisplayPage.jsx
+++ client/src/pages/ProductDisplayPage.jsx
@@ -164,7 +164,7 @@ const ProductDisplayPage = () => {
               }
            
 
-            <h2 className='font-semibold'>Why shop from Shopray? </h2>
+            <h2 className='font-semibold'>Why shop from Luxuries and Perfumes? </h2>
             <div>
                   <div className='flex  items-center gap-4 my-4'>
                       <img
